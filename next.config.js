237bae7a0dd026ca2aa62e46521606bc31c5317b
Rofiyanto <rofiyanto.pradikta@idntimes.com>
2022-09-04 21:09:16 +0700

    fix: hot reload

--- next.config.js
+++ next.config.js
@@ -10,5 +10,12 @@ module.exports = {
         permanent: false
       }
     ];
+  },
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300
+    };
+    return config;
   }
 };
